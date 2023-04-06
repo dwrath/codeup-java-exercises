8137aee2dcaea485cfd1637d7152914438f7e473
Damian Wrather <damian.wrather@gmail.com>
2023-04-06 13:33:05 -0500

    syntax exercise

---
+++ src/SyntaxExcercise.java
@@ -0,0 +1,40 @@
+public class SyntaxExcercise {
+    public static void main(String[] args) {
+//        int favoriteNumber = 5;
+//        System.out.println(favoriteNumber);
+//        String myString = "My String";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14F;
+////        int x = 5;
+////        System.out.println(x++);
+////        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+
+
+
+
+    }
+}
