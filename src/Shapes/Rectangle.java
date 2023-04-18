d11a89a2e64267b06c91bd8c6a686d22de616855
Damian Wrather <damian.wrather@gmail.com>
2023-04-18 11:16:31 -0500

    Interface and abstract classes

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -6,6 +6,8 @@ public class Rectangle extends Quadrilateral implements Measurable{
 
     public Rectangle(double length, double width) {
         super(length, width);
+        this.length = length;
+        this.width = width;
     }
 
 
