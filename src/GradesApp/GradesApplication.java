2c00f28f93d05a6d6b87035bc7182d9d671cbe18
Damian Wrather <damian.wrather@gmail.com>
2023-04-19 14:58:33 -0500

    grades and students

--- src/GradesApp/GradesApplication.java
+++ src/GradesApp/GradesApplication.java
@@ -3,6 +3,7 @@ package GradesApp;
 import grades.Student;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
     public static void main(String[] args) {
@@ -36,7 +37,30 @@ public class GradesApplication {
         Damarcus.addGrade(98);
         students.put("go_damarcus", Damarcus);
 
-        students.forEach((key, value) -> System.out.println(key + " : " + value.toString()));
+//        students.forEach((key, value) -> System.out.println(key + " : " + value.toString()));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome!\n Here are the GitHub usernames of our students:");
+        students.forEach((key, value) -> System.out.print("|" + key + "| "));
 
+        while (true) {
+            System.out.println("\n\nWhat student would you like to see more information on or enter 'all' for all class grades?\n");
+            String userInput = scanner.nextLine();
+            if (userInput.equals("all")) {
+                students.forEach((key, value) -> System.out.println(key + " : " + value.toString()));
+            } else {
+                while (!students.containsKey(userInput)) {
+                    System.out.printf("Sorry, no student found with the GitHub username of %s%n", userInput);
+                    userInput = scanner.nextLine();
+                }
+                if (students.containsKey(userInput)) {
+                    System.out.printf("Name: %s -Github Username: %s%n Current Average:%s%n Grades: %s%n", students.get(userInput).getName(), userInput, students.get(userInput).getGradeAverage(), students.get(userInput).toString());
+                    System.out.println("Would you like to see another student?");
+                    if (scanner.nextLine().equals("n")) {
+                        System.out.println("Goodbye, and have a wonderful day!");
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -5,8 +5,8 @@ import util.Input;
 import java.util.ArrayList;
 
 public class Student {
-    private String name;
-    private ArrayList<Integer> grades;
+    private final String name;
+    private final ArrayList<Integer> grades;
 
     public Student(String name){
         this.name = name;
